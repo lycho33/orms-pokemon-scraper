52a307261396477f90af6a653763874882f5cda0
lycho33 <cho.lyds@gmail.com>
2021-03-12 12:12:49 -0500

    commit tree 0e3a05d8052df2901aa951c6cb792ae470b2bc94

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,21 @@
 class Pokemon
+    attr_accessor :name, :type, :db
+    attr_reader :id
+
+    def initialize(id:, name:, type:, db:)
+      @id = id
+      @name = name
+      @type = type
+      @db = db
+    end
+      
+    def self.find(id, db)
+     d = db.execute("SELECT * FROM pokemon WHERE id = ? ", id)[0]
+     self.new(id: d[0], name: d[1], type: d[2], db: db)
+    end
+      
+    def self.save(name, type, db)
+      db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
+      @id = db.execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
+    end
 end
